1b1574a5f74c794b0aca5ab256bcf462815cbb6f
Praful01 <78860906+Praful010101@users.noreply.github.com>
2023-05-16 13:12:52 +0530

    initial

---
+++ sketch.js
@@ -0,0 +1,100 @@
+let nets = {};
+let modelNames = ['la_muse', 'rain_princess', 'udnie', 'wreck', 'scream', 'wave', 'mathura', 'fuchun', 'zhangdaqian'];
+let inputImg, styleImg;
+let outputImgData;
+let outputImg;
+let modelNum = 0;
+let currentModel = 'wave';
+let uploader;
+let isLoading = true;
+let isSafa = false;
+
+function setup() {
+  isSafa = isSafari();
+  if (isSafa) {
+    alert('Sorry we do not yet support your device, please open this page with Chrome on a desktop. We will support other devices in the near future!');
+    return;
+  }
+
+  noCanvas();
+  inputImg = select('#input-img').elt;
+  styleImg = select('#style-img').elt;
+
+  // load models
+  modelNames.forEach(n => {
+    nets[n] = new ml5.TransformNet('models/' + n + '/', modelLoaded);
+  });
+
+  // Image uploader
+  uploader = select('#uploader').elt;
+  uploader.addEventListener('change', gotNewInputImg);
+
+  // output img container
+  outputImgContainer = createImg('images/loading.gif', 'image');
+  outputImgContainer.parent('output-img-container');
+}
+
+// A function to be called when the model has been loaded
+function modelLoaded() {
+  modelNum++;
+  if (modelNum >= modelNames.length) {
+    modelReady = true;
+    predictImg(currentModel);
+  }
+}
+
+function predictImg(modelName) {
+  isLoading = true;
+  if (!modelReady) return;
+  if (inputImg) {
+    outputImgData = nets[modelName].predict(inputImg);
+  }
+  outputImg = ml5.array3DToImage(outputImgData);
+  outputImgContainer.elt.src = outputImg.src;
+  isLoading = false;
+}
+
+function updateStyleImg(ele) {
+  if (ele.src) {
+    styleImg.src = ele.src;
+    currentModel = ele.id;
+  }
+  if (currentModel) {
+    predictImg(currentModel);
+  }
+}
+
+function updateInputImg(ele) {
+  if (ele.src) inputImg.src = ele.src;
+  predictImg(currentModel);
+}
+
+function uploadImg() {
+  uploader.click();
+}
+
+function gotNewInputImg() {
+  if (uploader.files && uploader.files[0]) {
+    let newImgUrl = window.URL.createObjectURL(uploader.files[0]);
+    inputImg.src = newImgUrl;
+    inputImg.style.width = '250px';
+    inputImg.style.height = '250px';
+  }
+}
+
+function onPredictClick() {
+  predictImg(currentModel);
+}
+
+function isSafari() {
+  varua = navigator.userAgent.toLowerCase();
+  if (ua.indexOf('safari') != -1) {
+  if (ua.indexOf('chrome') > -1) {
+  return false;
+  } else {
+  return true;
+  }
+  }
+  }
+  
+  
